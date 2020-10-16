db8b001cf6d832f1a76701fc2aba6c10261eeccc
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-16 18:38:42 +0100

    Added a second view, user can now view how many days until their birthday

--- app.rb
+++ app.rb
@@ -1,9 +1,14 @@
 require 'sinatra'
-require 'shotgun'
 
 class Birthday < Sinatra::Base
   
   get '/' do
     erb(:index)
+  end
+
+  post '/birthday' do
+    @birthday = Date.parse(params[:user_birthday])
+    @bdate = Date.new(Date.today.year, @birthday.month, @birthday.day)
+    erb :birthday
   end
 end 
--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -4,3 +4,13 @@ feature 'Home page' do
     expect(page).to have_content ('Hey cutie! When is your birthday?')
   end
 end
+
+feature '/birthday' do
+  scenario 'User fills in the forms and submits' do
+    visit('/')
+   fill_in "Name",	with: "Beca"
+    fill_in "user_birthday", with: "02/03/1989"
+    click_button('Submit')
+    expect(page).to have_content ('Your birthday is in 137 days') 
+  end
+end
