3d3ab53ca3e713207f60e3dc956aa011e0198f51
Rebekah Galliano <rebekahgalliano@gmail.com>
2020-10-16 19:20:50 +0100

    Made the birthday in x.days more personal by adding @name (also started stylising with CSS)

--- spec/features/feature_spec.rb
+++ spec/features/feature_spec.rb
@@ -1,7 +1,7 @@
 feature 'Home page' do
   scenario 'User enters homepage' do
     visit('/')
-    expect(page).to have_content ('Hey cutie! When is your birthday?')
+    expect(page).to have_content ("Hey cutie!\nWhen is your birthday?\n")
   end
 end
 
@@ -10,15 +10,15 @@ feature '/birthday' do
     visit('/')
    fill_in "Name",	with: "Beca"
     fill_in "user_birthday", with: "02/03/1989"
-    click_button('Submit')
-    expect(page).to have_content ('Your birthday is in 137 days') 
+    click_button("Submit")
+    expect(page).to have_content ("Beca your birthday is in 137 days") 
   end
 
   scenario "User's birtday is today" do
     visit('/')
     fill_in "Name", with: "Beca"
     fill_in "user_birthday", with: "16/10/1989"
-    click_button('Submit')
+    click_button("Submit")
     expect(page).to have_content ("Happy Birthday Beca!") 
   end 
 end
